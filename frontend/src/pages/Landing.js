685cda7bfa324ba68a483248ff3da8d09d758aee
Carl Shen <carl.shen1@uwaterloo.ca>
2021-01-16 17:08:55 -0500

    More frontend

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import flowerDoc from "../flowerDoc3.png"
+import demoGif from "../demoGif2.gif"
 
+import Convert from "./Convert";
 // Landing/homepage
 class Landing extends Component {
   render() {
     return (
-      <div className="container-fluid poppin bg-dark text-light">
+        <div>
+      <div className="container-fluid poppin">
         <div className="row align-items-center" style={{textAlign:"center",verticalAlign: "middle"}}>
-            <div className="col-lg-6" style={{height:"100vh",verticalAlign: "middle",textAlign:"Left",padding:"20vh 5vw 0px 5vw"}}>
+            <div className="col-lg-12" style={{height:"100%",verticalAlign: "middle",textAlign:"Center",padding:"10vh 5vw 0px 5vw"}}>
               <h1 style={{fontSize:45,fontWeight:700}}>Web Development Made Easy.</h1>
               <p>
-                <br/>
-                <b>NAME</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                <br/><br/>
-                Tagline to be inserted Here. Tagline Here.
+                <b>DocSite</b> converts Google Docs into full fledged webpages, so anyone can build their own websites.
+                
+             
               </p>
+               <img src={flowerDoc} style={{height:"330px"}}/>
+              <img src={demoGif} style={{height:"300px",boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}  />
+                
               <Link
                 to="/convert"
                 style={{
@@ -27,10 +33,11 @@ class Landing extends Component {
                 Get Started
               </Link>
             </div>
-            <div className="col-lg-6 align-middle">
-              
-            </div>
           </div>
+
+
+        </div>
+        {/* <Convert/> */}
         </div>
     );
   }
